2072e234d5296234b7d99d0b1cd9626742f0c73f
cvelth <cvelth.mail@gmail.com>
2018-10-17 23:33:09 +0300

    NeuralNetworkTrainer was rewritten for mnn v2.0

--- NeuralNetworkTrainer/main.cpp
+++ NeuralNetworkTrainer/main.cpp
@@ -2,14 +2,17 @@
 #include <iostream>
 #include <sstream>
 #include <filesystem>
-#include "..\GameCore\GameCore.hpp"
-#include "..\GameCore\Field.hpp"
-#include "..\..\MyNeuralNetwork\NetworkEvolutionManager\NetworkGenerationEvolutionManager.hpp"
-#include "..\..\MyNeuralNetwork\AbstractNetworkCore\Storage.hpp"
+#include <random>
+#include "../GameCore/GameCore.hpp"
+#include "../GameCore/Field.hpp"
+#include "mnn/NeuralNetworkEvolutionManager/NeuralNetworkEvolutionManager.hpp"
+#include "mnn/MatrixLayeredNeuralNetwork/NeuralNetwork.hpp"
+#include "mnn/storage/storage.hpp"
 int main(int argc, char** argv) {
-	size_t generations = 10, generated_networks = 100, stored_networks = 5;
-	size_t hidden_neurons = 32, hidden_layers = 2;
-	float unit_mutation_chance = 0.2f, link_mutation_chance = 0.5f;
+	size_t generations = 10, generated_networks = 100;
+	size_t hidden_neurons = 64, hidden_layers = 64;
+	double survivability_ratio = 0.5;
+	double unit_mutation_chance = 0.2, link_mutation_chance = 0.5;
 	std::string folder_name;
 	std::stringstream s;
 	switch (argc) {
@@ -28,15 +31,15 @@ int main(int argc, char** argv) {
 			s.clear();
 		case 7:
 			s.str(argv[6]);
-			s >> hidden_layers;
+			s >> survivability_ratio;
 			s.clear();
 		case 6:
 			s.str(argv[5]);
-			s >> hidden_neurons;
+			s >> hidden_layers;
 			s.clear();
 		case 5:
 			s.str(argv[4]);
-			s >> stored_networks;
+			s >> hidden_neurons;
 			s.clear();
 		case 4:
 			s.str(argv[3]);
@@ -52,29 +55,50 @@ int main(int argc, char** argv) {
 					<< "\tFirst parameter - folder to store resultion networks.\n" << "\t\tNo default value\n"
 					<< "\tSecond parameter - number of generations.\n" << "\t\tDefault value: 10\n"
 					<< "\tThird parameter - number of networks per generation.\n" << "\t\tDefault value: 100\n"
-					<< "\tForth parameter - number of networks per generation to be stored.\n" << "\t\tDefault value: 5\n"
-					<< "\tFifth parameter - number of hidden neurons in each network.\n" << "\t\tDefault value: 32\n"
-					<< "\tSixth parameter - number of hidden layers in each network.\n" << "\t\tDefault value: 2\n"
-					<< "\tSeventh parameter - chance of unit mutation.\n" << "\t\tDefault value: 0.2\n"
-					<< "\tEighth parameter - chance of link mutation in mutated unit.\n" << "\t\tDefault value: 0.5\n";
+					<< "\tFourth parameter - maximum number of hidden neurons in each layer of a network.\n" << "\t\tDefault value: 64\n"
+					<< "\tFifth parameter - maximum number of hidden layers in each network.\n" << "\t\tDefault value: 64\n"
+					<< "\tSixth parameter - chance of unit mutation.\n" << "\t\tDefault value: 0.2\n"
+					<< "\tSeventh parameter - chance of link mutation in mutated unit.\n" << "\t\tDefault value: 0.5\n";
 				return 0;
 			} else
 				folder_name = argv[1];
 	}
 
 	if (folder_name != "") {
-		GameCore<4> core;
-		mnn::NetworkGenerationEvolutionManager em(generated_networks, [&core](std::function<NeuronContainer<Type>(NeuronContainer<Type>)> calculate) -> Type {
-			core.restart();
-			while (!core.isOver()) {
-				auto f = core.getField()->getNormalizedCellValues();
-				auto o = calculate(f);
-				if (!core.make_move(o[0], o[1], o[2], o[3])) break;
-			}
-			return Type(core.score());
-		}, 16, 4, hidden_neurons, hidden_layers);
 
-		em.newPopulation();
+		std::mt19937_64 g(std::random_device{}());
+		std::uniform_int_distribution<size_t> d_l(1, hidden_layers);
+		std::uniform_int_distribution<size_t> d_n(1, hidden_neurons);
+
+		mnn::NeuralNetworkEvolutionManager em {
+			generated_networks, 16, 4,
+			[](auto n) -> auto {
+				GameCore<4> core(0);
+				while (!core.isOver()) {
+					auto f = core.getField()->getNormalizedCellValues();
+					n->process(f);
+					auto o = n->outputs();
+					if (!core.make_move(float(o[0]), float(o[1]), float(o[2]), float(o[3]))) break;
+				}
+				return mnn::Value(core.score());
+			},
+			[&g, &d_l, &d_n]() -> auto { 
+				auto ret = std::make_shared<mnn::MatrixLayeredNeuralNetwork>(16, 4);
+				auto layer_counter = d_l(g);
+				while (--layer_counter)
+					ret->add_layer(d_n(g));
+				ret->add_layer(4);
+				return ret;
+			},
+			[](auto n1, auto n2) -> auto { 
+				return mnn::MatrixLayeredNeuralNetwork::generate(
+					*std::dynamic_pointer_cast<mnn::MatrixLayeredNeuralNetwork>(n1), 
+					*std::dynamic_pointer_cast<mnn::MatrixLayeredNeuralNetwork>(n2)
+				);
+			}
+		};
+
+		em.fill();
 		try {
 			std::experimental::filesystem::remove_all("./" + folder_name);
 			std::experimental::filesystem::create_directory(folder_name);
@@ -86,19 +110,16 @@ int main(int argc, char** argv) {
 				auto filename = s.str();
 				s.str("");
 
-				float average = em.testPopulation(true);
+				em.select();
+				//float average = em.select(true);
 				size_t j = 0;
 				for (auto it : *em) {
-					s << j++ << "_" << it.first << ".mnn";
-					mnn::save_to_file(filename + s.str(), it.second);
+					s << j++ << "_" << it.second << ".mnn";
+					mnn::save_to_file(filename + s.str(), *it.first);
 					s.str("");
-					if (j >= stored_networks)
-						break;
 				}
-				std::cout << "Generation_" << i << ":\t" << em->front().first << " <- " << average << " <- " << em->back().first << "\n";
-				em.populationSelection();
-				em.recreatePopulation();
-				em.mutatePopulation(unit_mutation_chance, link_mutation_chance);
+				std::cout << "Generation_" << i << ":\t" << em->front().second << " <- " << /*average << " <- " <<*/ em->back().second << "\n";
+				em.mutate(unit_mutation_chance, link_mutation_chance);
 			}
 		} catch (std::exception &e) {
 			std::cout << e.what();
